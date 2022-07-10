7c62a4a27f6e1e953b9ae6add3babc8fa5b617a9
任逍遥 <dsbssb@qq.com>
2022-07-10 21:56:24 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// import { ECharts } from 'echarts'
+import * as echarts from 'echarts'
 import './plugins/element.js'
 import '@/assets/css/index.less'
 // import '@/assets/css/blank-px.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
 
 new Vue({
   router,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,7 +59,7 @@
             </div>
           </el-col>
         </el-row>
-        <el-row :gutter="20" style="margin-top:15px">
+        <el-row :gutter="20" style="margin-top: 15px">
           <el-col :span="8">
             <div class="card">
               <div style="">
@@ -94,6 +94,18 @@
             </div>
           </el-col>
         </el-row>
+
+        <el-card style="margin-top: 15px">
+          <div class="chart_line" ref="chart_line" style="width: 100%; height: 34vh"></div>
+        </el-card>
+        <div style="margin-top: 15px; display: flex">
+          <el-card style="flex: 48%; margin-right: 20px">
+            <div class="chart_line" ref="chart_bar" style="width: 100%; height: 29vh"></div>
+          </el-card>
+          <el-card style="flex: 48%">
+            <div class="chart_line" ref="chart_pic" style="width: 100%; height: 29vh"></div>
+          </el-card>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -126,13 +138,153 @@ export default {
           name: '王小虎',
           address: '上海市普陀区金沙江路 1516 弄'
         }
-      ]
+      ],
+      chartArr: []
     }
   },
 
-  mounted() {},
+  mounted() {
+    this.$nextTick(function () {
+      this.init()
+    })
+  },
 
-  methods: {}
+  methods: {
+    init() {
+      this.createLine()
+      this.createBar()
+      this.createPic()
+    },
+    createChart(dom, option) {
+      const myChart = this.$echarts.init(dom)
+      myChart.setOption(option)
+      window.addEventListener('resize', function () {
+        myChart.resize()
+      })
+      this.chartArr.push(myChart)
+    },
+    createLine() {
+      const option = {
+        backgroundColor: '',
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+        },
+        grid: {
+          left: 0,
+          right: '1%',
+          top: '15%',
+          bottom: 0,
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: 'Email',
+            type: 'line',
+            stack: 'Total',
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: 'Union Ads',
+            type: 'line',
+            stack: 'Total',
+            data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+            name: 'Video Ads',
+            type: 'line',
+            stack: 'Total',
+            data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+            name: 'Direct',
+            type: 'line',
+            stack: 'Total',
+            data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+            name: 'Search Engine',
+            type: 'line',
+            stack: 'Total',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+        ]
+      }
+      this.createChart(this.$refs.chart_line, option)
+    },
+    createBar() {
+      const option = {
+        backgroundColor: '',
+        legend: {},
+        tooltip: {},
+        grid: {
+          left: 0,
+          right: 0,
+          top: '15%',
+          bottom: 0,
+          containLabel: true
+        },
+        dataset: {
+          source: [
+            ['product', '2015', '2016', '2017'],
+            ['Matcha Latte', 43.3, 85.8, 93.7],
+            ['Milk Tea', 83.1, 73.4, 55.1],
+            ['Cheese Cocoa', 86.4, 65.2, 82.5],
+            ['Walnut Brownie', 72.4, 53.9, 39.1]
+          ]
+        },
+        xAxis: { type: 'category' },
+        yAxis: {},
+        // Declare several bar series, each will be mapped
+        // to a column of dataset.source by default.
+        series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' }]
+      }
+      this.createChart(this.$refs.chart_bar, option)
+    },
+    createPic() {
+      const option = {
+        backgroundColor: '',
+        legend: {
+          show: false,
+          top: 'top'
+        },
+        grid: {},
+        series: [
+          {
+            name: 'Nightingale Chart',
+            type: 'pie',
+            radius: ['14%', '80%'],
+            center: ['50%', '50%'],
+            roseType: 'area',
+            itemStyle: {
+              borderRadius: 8
+            },
+            data: [
+              { value: 40, name: 'rose 1' },
+              { value: 38, name: 'rose 2' },
+              { value: 32, name: 'rose 3' },
+              { value: 30, name: 'rose 4' },
+              { value: 28, name: 'rose 5' },
+              { value: 26, name: 'rose 6' },
+              { value: 22, name: 'rose 7' },
+              { value: 18, name: 'rose 8' }
+            ]
+          }
+        ]
+      }
+      this.createChart(this.$refs.chart_pic, option)
+    }
+  }
 }
 </script>
 
